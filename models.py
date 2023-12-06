978f7bdc5f1b6061db1c73138dccc5c6d1d00483
Szymon Żuk <116501563+szymon-zuk@users.noreply.github.com>
2023-12-06 13:03:14 +0000

    models

---
+++ models.py
@@ -0,0 +1,7 @@
+from database import Base
+from sqlalchemy import Column, Integer, String
+
+class Blog(Base):
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    body = Column(String)
