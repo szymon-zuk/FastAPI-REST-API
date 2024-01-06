ff227b5315af4c0ece4941a0a04650062af0d609
szymon-zuk <szymon.p.zuk@gmail.com>
2024-01-06 12:48:44 +0100

    delete old project

--- app/models.py
+++
@@ -1,7 +0,0 @@
-from database import Base
-from sqlalchemy import Column, Integer, String
-
-class Blog(Base):
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String)
-    body = Column(String)
