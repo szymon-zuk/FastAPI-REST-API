4007b06665e76f379647d430d7148dbf56be55d4
szymon-zuk <szymon.p.zuk@gmail.com>
2024-01-09 18:50:14 +0100

    get current user function - jwt decode

--- ToDoApp/routers/auth.py
+++ ToDoApp/routers/auth.py
@@ -1,14 +1,14 @@
 from datetime import timedelta, datetime
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from pydantic import BaseModel
 from starlette import status
-from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from models import Users
 from passlib.context import CryptContext
 from database import SessionLocal
 from typing import Annotated
 from sqlalchemy.orm import Session
-from jose import jwt
+from jose import jwt, JWTError
 
 router = APIRouter()
 
@@ -16,6 +16,7 @@ SECRET_KEY = '8d9810dfd755d8dab705f570cee8701256dea433928c5236818f2ce880173927'
 ALGORITHM = 'HS256'
 
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl='token')
 
 
 class CreateUserRequest(BaseModel):
@@ -57,6 +58,18 @@ def create_access_token(username: str, user_id: int, expires_delta: timedelta):
     expires = datetime.utcnow() + expires_delta
     encode.update({'exp': expires})
     return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get('sub')
+        user_id: int = payload.get('id')
+        if username is None or user_id is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could not validate user')
+        return {"username": username, "id": user_id}
+    except JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could not validate user')
 
 
 @router.post("/auth", status_code=status.HTTP_201_CREATED)
