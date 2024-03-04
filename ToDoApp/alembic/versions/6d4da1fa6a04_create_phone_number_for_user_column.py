7ae760d84341ad4636fdac660020c57ce44f765a
szymon-zuk <szymon.p.zuk@gmail.com>
2024-03-04 19:06:20 +0100

    first alembic migration - done

---
+++ ToDoApp/alembic/versions/6d4da1fa6a04_create_phone_number_for_user_column.py
@@ -0,0 +1,26 @@
+"""create phone number for user column
+
+Revision ID: 6d4da1fa6a04
+Revises: 
+Create Date: 2024-03-04 19:01:47.278203
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "6d4da1fa6a04"
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.add_column("users", sa.Column("phone_number", sa.String(), nullable=True))
+
+
+def downgrade() -> None:
+    pass
