f5c9e0e74a317d2c5185cb3d3fc9ed2f265e2b5d
szymon-zuk <szymon.p.zuk@gmail.com>
2024-06-02 21:25:48 +0200

    tests

--- ToDoApp/test/test_main.py
+++ ToDoApp/test/test_main.py
@@ -4,6 +4,7 @@ from fastapi import status
 
 client = TestClient(app)
 
+
 def test_return_health_check():
     response = client.get("/healthy")
     assert response.status_code == status.HTTP_200_OK
--- ToDoApp/test/test_todos.py
+++ ToDoApp/test/test_todos.py
@@ -72,3 +72,40 @@ def test_read_all_authenticated(test_todo):
             "owner_id": 1,
         }
     ]
+
+
+def test_read_one_authenticated(test_todo):
+    response = client.get("/todo/1")
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {
+        "complete": False,
+        "title": "Nauka FastAPI",
+        "description": "codziennie",
+        "id": 1,
+        "priority": 5,
+        "owner_id": 1,
+    }
+
+
+def test_read_one_authenticated_not_found(test_todo):
+    response = client.get("/todo/999")
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert response.json() == {"detail": "Todo not found"}
+
+
+def test_create_todo(test_todo):
+    request_data = {
+        "title": "Nauka FastAPI",
+        "description": "codziennie",
+        "priority": 5,
+        "complete": False,
+    }
+
+    response = client.post("/todo", json=request_data)
+    assert response.status_code == status.HTTP_201_CREATED
+    db = TestingSessionLocal()
+    model = db.query(ToDos).filter(ToDos.id == 2).first()
+    assert model.title == request_data.get("title")
+    assert model.description == request_data.get("description")
+    assert model.priority == request_data.get("priority")
+    assert model.complete == request_data.get("complete")
