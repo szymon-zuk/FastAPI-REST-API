1aaed462aa4ca6a72b859fd38160862b16a543a7
szymon-zuk <szymon.p.zuk@gmail.com>
2024-08-10 13:35:42 +0200

    Added endpoints for displaying templates

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -1,20 +1,22 @@
-import sys
-sys.path.append("..")
-
-from fastapi import Depends, HTTPException, status, APIRouter
-from pydantic import BaseModel
-from typing import Optional
-import models
-from passlib.context import CryptContext
-from sqlalchemy.orm import Session
-from database import SessionLocal, engine
-from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from datetime import datetime, timedelta
-from jose import jwt, JWTError
+from typing import Optional
 
+from fastapi import APIRouter, Depends, HTTPException, Request, status
+from fastapi.responses import HTMLResponse
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi.templating import Jinja2Templates
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
+
+from TodoApp import models
+from TodoApp.database import SessionLocal, engine
 
 SECRET_KEY = "KlgH6AzYDeZeGwD288to79I3vTHT8wp7"
 ALGORITHM = "HS256"
+
+templates = Jinja2Templates(directory="templates")
 
 
 class CreateUser(BaseModel):
@@ -31,11 +33,10 @@ models.Base.metadata.create_all(bind=engine)
 
 oauth2_bearer = OAuth2PasswordBearer(tokenUrl="token")
 
-
 router = APIRouter(
     prefix="/auth",
     tags=["auth"],
-    responses={401: {"user": "Not authorized"}}
+    responses={401: {"user": "Not authorized"}},
 )
 
 
@@ -47,39 +48,36 @@ def get_db():
         db.close()
 
 
-def get_password_hash(password):
+def get_password_hash(password: str) -> str:
     return bcrypt_context.hash(password)
 
 
-def verify_password(plain_password, hashed_password):
+def verify_password(plain_password: str, hashed_password: str) -> bool:
     return bcrypt_context.verify(plain_password, hashed_password)
 
 
-def authenticate_user(username: str, password: str, db):
-    user = db.query(models.Users)\
-        .filter(models.Users.username == username)\
-        .first()
+def authenticate_user(
+    username: str, password: str, db: Session
+) -> Optional[models.Users]:
+    user = db.query(models.Users).filter(models.Users.username == username).first()
 
-    if not user:
-        return False
-    if not verify_password(password, user.hashed_password):
+    if not user or not verify_password(password, user.hashed_password):
         return False
     return user
 
 
-def create_access_token(username: str, user_id: int,
-                        expires_delta: Optional[timedelta] = None):
-
+def create_access_token(
+    username: str, user_id: int, expires_delta: Optional[timedelta] = None
+) -> str:
     encode = {"sub": username, "id": user_id}
-    if expires_delta:
-        expire = datetime.utcnow() + expires_delta
-    else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
+    expire = datetime.utcnow() + (
+        expires_delta if expires_delta else timedelta(minutes=15)
+    )
     encode.update({"exp": expire})
     return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
 
 
-async def get_current_user(token: str = Depends(oauth2_bearer)):
+async def get_current_user(token: str = Depends(oauth2_bearer)) -> dict:
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get("sub")
@@ -93,60 +91,53 @@ async def get_current_user(token: str = Depends(oauth2_bearer)):
 
 @router.post("/create/user")
 async def create_new_user(create_user: CreateUser, db: Session = Depends(get_db)):
-    create_user_model = models.Users()
-    create_user_model.email = create_user.email
-    create_user_model.username = create_user.username
-    create_user_model.first_name = create_user.first_name
-    create_user_model.last_name = create_user.last_name
-
-    hash_password = get_password_hash(create_user.password)
-
-    create_user_model.hashed_password = hash_password
-    create_user_model.is_active = True
+    create_user_model = models.Users(
+        email=create_user.email,
+        username=create_user.username,
+        first_name=create_user.first_name,
+        last_name=create_user.last_name,
+        hashed_password=get_password_hash(create_user.password),
+        is_active=True,
+    )
 
     db.add(create_user_model)
     db.commit()
 
 
 @router.post("/token")
-async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(),
-                                 db: Session = Depends(get_db)):
+async def login_for_access_token(
+    form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)
+):
     user = authenticate_user(form_data.username, form_data.password, db)
     if not user:
         raise token_exception()
     token_expires = timedelta(minutes=20)
-    token = create_access_token(user.username,
-                                user.id,
-                                expires_delta=token_expires)
+    token = create_access_token(user.username, user.id, expires_delta=token_expires)
     return {"token": token}
 
 
-#Exceptions
-def get_user_exception():
-    credentials_exception = HTTPException(
+@router.get("/", response_class=HTMLResponse)
+async def authentication_page(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
+
+
+@router.get("/register", response_class=HTMLResponse)
+async def register(request: Request):
+    return templates.TemplateResponse("register.html", {"request": request})
+
+
+# Exceptions
+def get_user_exception() -> HTTPException:
+    return HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
         headers={"WWW-Authenticate": "Bearer"},
     )
-    return credentials_exception
 
 
-def token_exception():
-    token_exception_response = HTTPException(
+def token_exception() -> HTTPException:
+    return HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Incorrect username or password",
         headers={"WWW-Authenticate": "Bearer"},
     )
-    return token_exception_response
-
-
-
-
-
-
-
-
-
-
-
-
