ce7487e0b0dd4d3bfb6336992047d60d8bd5cf8f
szymon-zuk <szymon.p.zuk@gmail.com>
2024-08-12 22:18:32 +0200

    Modified login

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -1,9 +1,7 @@
 from datetime import datetime, timedelta
 from typing import Optional
 
-import models
-from database import SessionLocal, engine
-from fastapi import APIRouter, Depends, HTTPException, Request, status
+from fastapi import APIRouter, Depends, HTTPException, Request, Response, status
 from fastapi.responses import HTMLResponse
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from fastapi.templating import Jinja2Templates
@@ -11,6 +9,10 @@ from jose import JWTError, jwt
 from passlib.context import CryptContext
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
+from starlette.responses import RedirectResponse
+
+import models
+from database import SessionLocal, engine
 
 SECRET_KEY = "KlgH6AzYDeZeGwD288to79I3vTHT8wp7"
 ALGORITHM = "HS256"
@@ -37,6 +39,18 @@ router = APIRouter(
     tags=["auth"],
     responses={401: {"user": "Not authorized"}},
 )
+
+
+class LoginForm:
+    def __init__(self, request: Request):
+        self.request: Request = request
+        self.username: Optional[str] = None
+        self.password: Optional[str] = None
+
+    async def create_oauth_form(self):
+        form = await self.request.form()
+        self.username = form.get("email")
+        self.password = form.get("password")
 
 
 def get_db():
@@ -76,13 +90,16 @@ def create_access_token(
     return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
 
 
-async def get_current_user(token: str = Depends(oauth2_bearer)) -> dict:
+async def get_current_user(request: Request):
     try:
+        token = request.cookies.get("access_token")
+        if token is None:
+            return None
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get("sub")
         user_id: int = payload.get("id")
         if username is None or user_id is None:
-            raise get_user_exception()
+            return None
         return {"username": username, "id": user_id}
     except JWTError:
         raise get_user_exception()
@@ -105,19 +122,45 @@ async def create_new_user(create_user: CreateUser, db: Session = Depends(get_db)
 
 @router.post("/token")
 async def login_for_access_token(
-    form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)
+    response: Response,
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    db: Session = Depends(get_db),
 ):
     user = authenticate_user(form_data.username, form_data.password, db)
     if not user:
-        raise token_exception()
-    token_expires = timedelta(minutes=20)
+        return False
+    token_expires = timedelta(minutes=60)
     token = create_access_token(user.username, user.id, expires_delta=token_expires)
-    return {"token": token}
+    response.set_cookie(key="access_token", value=token, httponly=True)
+    return True
 
 
 @router.get("/", response_class=HTMLResponse)
 async def authentication_page(request: Request):
     return templates.TemplateResponse("login.html", {"request": request})
+
+
+@router.post("/", response_class=HTMLResponse)
+async def login(request: Request, db: Session = Depends(get_db)):
+    try:
+        form = LoginForm(request)
+        await form.create_oauth_form()
+        response = RedirectResponse(url="/todos", status_code=status.HTTP_302_FOUND)
+
+        validate_user_cookie = await login_for_access_token(
+            response=response, form_data=form, db=db
+        )
+        if not validate_user_cookie:
+            message = "Incorrect username or password"
+            return templates.TemplateResponse(
+                "login.html", {"request": request, "message": message}
+            )
+        return response
+    except HTTPException:
+        message = "Unknown error"
+        return templates.TemplateResponse(
+            "login.html", {"request": request, "message": message}
+        )
 
 
 @router.get("/register", response_class=HTMLResponse)
--- TodoApp/routers/todos.py
+++ TodoApp/routers/todos.py
@@ -8,6 +8,7 @@ from starlette.responses import RedirectResponse
 
 import models
 from database import SessionLocal, engine
+from routers.auth import get_current_user
 
 router = APIRouter(
     prefix="/todos", tags=["todos"], responses={404: {"description": "Not found"}}
@@ -28,12 +29,18 @@ def get_db():
 
 @router.get("/", response_class=HTMLResponse)
 async def read_all_by_user(request: Request, db: Session = Depends(get_db)):
-    todos = db.query(models.Todos).filter(models.Todos.owner_id == 1).all()
+    user = await get_current_user(request)
+    if user is None:
+        return RedirectResponse(url="/auth", status_code=status.HTTP_302_FOUND)
+    todos = db.query(models.Todos).filter(models.Todos.owner_id == user.get("id")).all()
     return templates.TemplateResponse("home.html", {"request": request, "todos": todos})
 
 
 @router.get("/add-todo", response_class=HTMLResponse)
 async def add_todo(request: Request):
+    user = await get_current_user(request)
+    if user is None:
+        return RedirectResponse(url="/auth", status_code=status.HTTP_302_FOUND)
     return templates.TemplateResponse("add-todo.html", {"request": request})
 
 
@@ -45,12 +52,15 @@ async def create_todo(
     priority: int = Form(...),
     db: Session = Depends(get_db),
 ):
+    user = await get_current_user(request)
+    if user is None:
+        return RedirectResponse(url="/auth", status_code=status.HTTP_302_FOUND)
     todo_model = models.Todos()
     todo_model.title = title
     todo_model.description = description
     todo_model.priority = priority
     todo_model.complete = False
-    todo_model.owner_id = 1
+    todo_model.owner_id = user.get("id")
 
     db.add(todo_model)
     db.commit()
@@ -59,6 +69,9 @@ async def create_todo(
 
 @router.get("/edit-todo/{todo_id}", response_class=HTMLResponse)
 async def edit_todo(request: Request, todo_id: int, db: Session = Depends(get_db)):
+    user = await get_current_user(request)
+    if user is None:
+        return RedirectResponse(url="/auth", status_code=status.HTTP_302_FOUND)
     todo = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
     return templates.TemplateResponse(
         "edit-todo.html", {"request": request, "todo": todo}
@@ -74,6 +87,9 @@ async def edit_todo_commit(
     priority: int = Form(...),
     db: Session = Depends(get_db),
 ):
+    user = await get_current_user(request)
+    if user is None:
+        return RedirectResponse(url="/auth", status_code=status.HTTP_302_FOUND)
     todo_model = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
     todo_model.title = title
     todo_model.description = description
@@ -85,10 +101,13 @@ async def edit_todo_commit(
 
 @router.get("/delete/{todo_id}")
 async def delete_todo(request: Request, todo_id: int, db: Session = Depends(get_db)):
+    user = await get_current_user(request)
+    if user is None:
+        return RedirectResponse(url="/auth", status_code=status.HTTP_302_FOUND)
     todo_model = (
         db.query(models.Todos)
         .filter(models.Todos.id == todo_id)
-        .filter(models.Todos.owner_id == 1)
+        .filter(models.Todos.owner_id == user.get("id"))
         .first()
     )
     if todo_model is None:
@@ -103,6 +122,9 @@ async def delete_todo(request: Request, todo_id: int, db: Session = Depends(get_
 
 @router.get("/complete/{todo_id}", response_class=HTMLResponse)
 async def complete_todo(request: Request, todo_id: int, db: Session = Depends(get_db)):
+    user = await get_current_user(request)
+    if user is None:
+        return RedirectResponse(url="/auth", status_code=status.HTTP_302_FOUND)
     todo = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
 
     todo.complete = not todo.complete
