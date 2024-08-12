7382a49d1e6994e9a1851ebaf108443b49946933
szymon-zuk <szymon.p.zuk@gmail.com>
2024-08-12 17:49:15 +0200

    Edit and delete todo

--- TodoApp/routers/todos.py
+++ TodoApp/routers/todos.py
@@ -33,7 +33,7 @@ async def read_all_by_user(request: Request, db: Session = Depends(get_db)):
 
 
 @router.get("/add-todo", response_class=HTMLResponse)
-async def add_todo(request: Request, db: Session = Depends(get_db)):
+async def add_todo(request: Request):
     return templates.TemplateResponse("add-todo.html", {"request": request})
 
 
@@ -58,5 +58,44 @@ async def create_todo(
 
 
 @router.get("/edit-todo/{todo_id}", response_class=HTMLResponse)
-async def edit_todo(request: Request):
-    return templates.TemplateResponse("edit-todo.html", {"request": request})
+async def edit_todo(request: Request, todo_id: int, db: Session = Depends(get_db)):
+    todo = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
+    return templates.TemplateResponse(
+        "edit-todo.html", {"request": request, "todo": todo}
+    )
+
+
+@router.post("/edit-todo/{todo_id}", response_class=HTMLResponse)
+async def edit_todo_commit(
+    request: Request,
+    todo_id: int,
+    title: str = Form(...),
+    description: str = Form(...),
+    priority: int = Form(...),
+    db: Session = Depends(get_db),
+):
+    todo_model = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
+    todo_model.title = title
+    todo_model.description = description
+    todo_model.priority = priority
+    db.add(todo_model)
+    db.commit()
+    return RedirectResponse(url="/todos", status_code=status.HTTP_302_FOUND)
+
+
+@router.get("/delete/{todo_id}")
+async def delete_todo(request: Request, todo_id: int, db: Session = Depends(get_db)):
+    todo_model = (
+        db.query(models.Todos)
+        .filter(models.Todos.id == todo_id)
+        .filter(models.Todos.owner_id == 1)
+        .first()
+    )
+    if todo_model is None:
+        return RedirectResponse(url="/todos", status_code=status.HTTP_302_FOUND)
+
+    db.query(models.Todos).filter(models.Todos.id == todo_id).delete()
+
+    db.commit()
+
+    return RedirectResponse(url="/todos", status_code=status.HTTP_302_FOUND)
